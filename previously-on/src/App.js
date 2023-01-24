275f384a02b62163152bf7fcab3483e5ba2b6ff5
marius <marius.mestron@epitech.eu>
2023-01-24 17:05:08 +0100

    [ADD]: redirection sur la page home après l'auth2 && stockage du access_token dans un cookie

--- previously-on/src/App.js
+++ previously-on/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Welcome from "./components/Welcome.js";
 import GetToken from "./components/getToken.js";
+import Home from "./components/Home.js";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
 
         <Routes>
           <Route path="/" exact element={<Welcome />} />
+          <Route path="/Home" exact element={<Home />} />
           <Route path="/auth/get_token" exact element={<GetToken />} />
         </Routes>
 
--- previously-on/src/components/getToken.js
+++ previously-on/src/components/getToken.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import { useCookies } from 'react-cookie'
 import oauth from './requests/oauth.js';
 
 export default function GetToken() {
 
     const [token, setToken] = useState('');
+    const [cookies, setCookie] = useCookies(['access_token']);
 
     const myOAuth = new oauth();
     const params = new URLSearchParams(window.location.search);
@@ -12,9 +14,15 @@ export default function GetToken() {
 
     res.then(data => setToken(data.access_token));
 
+    let expires = new Date()
+    expires.setTime(expires.getTime() + (60 * 1000));
+    setCookie('access_token', token, { path: '/',  expires})
+
+    window.location = "http://vps-9741b19a.vps.ovh.net:3000/Home";
+
     return (
         <div>
-            {token ? <p>Access token: {token}</p> : <p>Error getting access token</p>}
+            <p>Authentification...</p>
         </div>
     );
 }
