9521e6a45b375784edc6b68bc4df4300e922ee66
marius <marius.mestron@epitech.eu>
2023-01-26 14:21:44 +0100

    [ADD]: affichage des détails de la série au moment d'un appuis long

--- previously-on/src/components/Home.js
+++ previously-on/src/components/Home.js
@@ -4,6 +4,18 @@ import home_style from "./style/home_style";
 
 export default function Home() {
     const [series, setSeries] = useState([]);
+    const [selectedSeries, setSelectedSeries] = useState(null);
+    const [startTime, setStartTime] = useState(null);
+
+    const handleMouseDown = () => {
+        setStartTime(Date.now());
+    };
+
+    const handleMouseUp = (s) => {
+        if (Date.now() - startTime > 500) {
+            setSelectedSeries(s);
+        }
+    };
 
     if (series.length < 1) {
         GetSeries().then(res => setSeries(res.data.shows));
@@ -16,13 +28,28 @@ export default function Home() {
             <div style={home_style.container}>
                 {series.length > 0 && series.map((s) => {
                     return (
-                        <div style={home_style.cards} key={s.id}>
-                            <img style={home_style.poster} alt={s.title} src={s.images.poster}/>
-                            <p>Titre : {s.title}</p>
-                            <p>Nombre de saisons : {s.seasons}</p>
-                            <p>Nombre d'épisodes : {s.episodes}</p>
-                            <p>Déscription : {s.description}</p>
-                            <p>Note : {s.notes.mean}</p>
+                        <div key={s.id}>
+                            <div style={home_style.cards} onMouseDown={handleMouseDown} onMouseUp={() => handleMouseUp(s)}>
+                                <div style={home_style.header_cards}>
+                                    <div>
+                                        <img style={home_style.poster} alt={s.title} src={s.images.poster} />
+                                    </div>
+                                    <div>
+                                        <p>Titre : {s.title}</p>
+                                        <p>Déscription : {s.description}</p>
+                                    </div>
+                                </div>
+                                {selectedSeries === s && (
+                                    <div>
+                                        <p>Genres : {Object.values(s.genres).join(', ')}</p>
+                                        <p>Nombre de saisons : {s.seasons}</p>
+                                        <p>Nombre d'épisodes : {s.episodes}</p>
+                                        <p>Durée moyenne d'un épisode : {s.length} minutes</p>
+                                        <p>Note : {s.notes.mean}</p>
+                                    </div>
+                                )}
+                            </div>
+
                         </div>
                     );
                 })}
--- previously-on/src/components/style/home_style.js
+++ previously-on/src/components/style/home_style.js
@@ -1,21 +1,29 @@
 const home_style = {
     poster: {
-        width: "30%"
+        width: "50%"
     },
+
     container: {
         width: "80%",
-        marginRight: "auto",                      
+        marginRight: "auto",
         marginLeft: "auto",
 
-       display: "flex",
-       flexDirection: "row",
-       flexWrap: "wrap",
-       gap: "1%"
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 1fr)",
+        gridTemplateRows: "auto",
+        gridGap: "1%"
     },
+
+
     cards: {
         border: "1px solid black",
         width: "25%"
-    },  
+    },
+
+    header_cards: {
+        display: "flex",
+        gap: "1%"
+    }
 }
 
 export default home_style;
